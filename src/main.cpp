c9aa43122fef379ae0d479ad1d92280fc3a60195
Xx_angel_xX <isawaduckflyonce@proton.me>
2025-09-05 23:58:46 +0200

    5.4 - Fragment shader
    Took 21 minutes

--- src/main.cpp
+++ src/main.cpp
@@ -3,15 +3,23 @@
 #include "GLFW/glfw3.h"
 
 // Version
-const char ver_num[] = "5.3";
-const char ver_name[] = "Compiling a shader";
+const char ver_num[] = "5.4";
+const char ver_name[] = "Fragment shader";
 
-// Temporal shader source code
+// Temporal vertex shader source code
 const char *vertexShaderSource = "#version 330 core\n"
 "layout (location = 0) in vec3 aPos;\n"
 "void main()\n"
 "{\n"
 " gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"}\0";
+
+// Temporal fragment shader source code
+const char *fragmentShaderSource = "#version 330 core\n"
+"out vec4 FragColor;\n"
+"void main()\n"
+"{\n"
+"    FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
 "}\0";
 
 // Callback functions
@@ -35,8 +43,8 @@ int main() {
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     // Creating window object:
-    GLFWwindow* window = glfwCreateWindow(800, 600, "OpenGL", NULL, NULL);
-    if (window == NULL)
+    GLFWwindow* window = glfwCreateWindow(800, 600, "OpenGL", nullptr, nullptr);
+    if (window == nullptr)
     {
         std::cout << "Failed to create GLFW window" << std::endl;
         glfwTerminate();
@@ -76,21 +84,40 @@ int main() {
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
     // Vertex shader creation
-    unsigned int vertexShader;
-    vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
 
-    // Shader source code attachment and compilation
-    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    // Vertex shader source code attachment and compilation
+    glShaderSource(vertexShader, 1, &vertexShaderSource, nullptr);
     glCompileShader(vertexShader);
 
-    // Checking for shader compilation errors
-    int success;
-    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
-    if(!success)
+    // Checking for vertex shader compilation errors
+    int VSsuccess;
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &VSsuccess);
+    if(!VSsuccess)
     {
         char infoLog[512];
-        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+        glGetShaderInfoLog(vertexShader, 512, nullptr, infoLog);
         std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" <<
+        infoLog << std::endl;
+
+        return -1;
+    }
+
+    // Fragment shader creation
+    unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+
+    // Fragment shader source code attachment and compilation
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, nullptr);
+    glCompileShader(fragmentShader);
+
+    // Checking for fragment shader compilation errors
+    int FSsuccess;
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &FSsuccess);
+    if(!FSsuccess)
+    {
+        char infoLog[512];
+        glGetShaderInfoLog(fragmentShader, 512, nullptr, infoLog);
+        std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" <<
         infoLog << std::endl;
 
         return -1;
